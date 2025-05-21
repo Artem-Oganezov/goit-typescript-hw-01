014aaa9873550fb03b56fdc6976087f74b36ef73
Artem-Oganezov <artem_oga@i.ua>
2025-05-21 15:09:47 +0300

    add file

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+let mixedType: string | number;
+let enableDisable: "enable" | "disable";
---
+++ src/generics/4.ts
@@ -0,0 +1,15 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  console.log("User created or updated:", initialValues);
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
